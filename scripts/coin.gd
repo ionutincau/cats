ef6fa46f4d6ca267b2bd92fa9d35d57354343f84
ionutincau <ionut.incau@gmail.com>
2025-04-06 23:37:42 +0300

    fixed score label

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,10 +1,9 @@
 extends Area2D
 
-@onready var game_manager = %game_manager
 @onready var animation_player = $AnimationPlayer
 
 var PICKUP_ANIMATION_NAME = "pickup"
 
 func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_coin()
+	GameManager.add_coin()
 	animation_player.play(PICKUP_ANIMATION_NAME)
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
-@onready var score_label = %score_label
-
+@onready var score_label = Ui.get_node("score_label")
+ 
 var coins = 0
+
+func _ready() -> void:
+	update_score_label()
 
 func add_coin():
 	coins += 1
-	score_label.text = "coins: " + str(coins)
+	update_score_label()
+
+
+func update_score_label():
+	score_label.text = "x" + str(coins)
