3bbac2fac9592faa4a8457fbfa9088b364657147
ionutincau <ionut.incau@gmail.com>
2025-04-17 10:40:16 +0300

    reposition elements

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,6 +4,6 @@ extends Area2D
 
 var PICKUP_ANIMATION_NAME = "pickup"
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	GameManager.add_coin()
 	animation_player.play(PICKUP_ANIMATION_NAME)
