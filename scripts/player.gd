cbb7e60cf16c7632585290eb7d120ec17ff7cd94
ionutincau <ionut.incau@gmail.com>
2025-04-17 16:30:51 +0300

    player flips

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
@@ -17,6 +18,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
