48357e10e181f49060556b53b3fdffb69ebe0b4a
ionutincau <ionut.incau@gmail.com>
2025-04-17 13:35:48 +0300

    add killzone

---
+++ scripts/killzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+@onready var game_over_sound: AudioStreamPlayer2D = $GameOverSound
+
+func _on_body_entered(_body: Node2D) -> void:
+	timer.start()
+	game_over_sound.play()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
