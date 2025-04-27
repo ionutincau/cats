d76347ec902f553b0793c40b827f554aa6bb5abb
ionutincau <ionut.incau@gmail.com>
2025-04-27 18:36:56 +0300

    fire on key press

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -4,8 +4,9 @@ extends Node2D
 @onready var player: CharacterBody2D = %Player
 @onready var bullet_scene = load("res://scenes/bullet.tscn")
 
-func _ready() -> void:
-	fire()
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("fire"):
+		fire()
 
 func fire():
 	var bullet = bullet_scene.instantiate()
